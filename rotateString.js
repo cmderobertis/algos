1986dbf3a738da496a2a34f69a58d6988d5a9a65
cmderobertis <cmderobertis@gmail.com>
2022-08-18 09:51:30 -0700

    added algo

--- rotateString.js
+++ rotateString.js
@@ -5,7 +5,14 @@ function rotateWord(str, int) {
   return sliceEnd + sliceStart
 }
 
-console.log(rotateWord(('Hello world'), 254))
-console.log(rotateWord(('Hello world'), 10))
-console.log(rotateWord(('Hello world'), 66))
-console.log(rotateWord(('Tyler is cool'), 42))
+// console.log(rotateWord(('Hello world'), 254))
+// console.log(rotateWord(('Hello world'), 10))
+// console.log(rotateWord(('Hello world'), 66))
+// console.log(rotateWord(('Tyler is cool'), 42))
+
+
+function isRotation(str1, str2) {
+  return (str1 + str1).includes(str2)
+}
+
+console.log(isRotation('ABCD', 'CDAB'))
