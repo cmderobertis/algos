18cf1f84cb8f4a1ca1fe6e056871a9212274902a
cmderobertis <cmderobertis@gmail.com>
2022-08-22 10:36:22 -0700

    added file to test language syntax

---
+++ test.js
@@ -0,0 +1,14 @@
+
+// Can JS objects have keys that are integers?
+obj = {
+  10: 23453,
+  13: 23
+}
+
+console.log(obj[10])
+// Yes!
+
+// Destructuring
+  // A new way to declare multiple variables at once.
+let [q, d, n, p] = [25, 10, 5, 1]
+console.log(q)
