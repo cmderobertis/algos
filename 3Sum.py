126b03524c2d8f30c041925eed7c7ae166e9a802
cmderobertis <cmderobertis@gmail.com>
2022-10-31 09:35:58 -0700

    working on 3Sum

--- 3Sum.py
+++ 3Sum.py
@@ -5,7 +5,7 @@ def threeSum(nums):
   for index1 in range(numsLength):
     for index2 in range(index1 + 1, numsLength):
       print(nums[index1], nums[index2])
-      target = nums[index1] + nums[index2]
+      target = -(nums[index1] + nums[index2])
       for index3 in range(numsLength):
         if nums[index3] + target == 0 and index3 != index1 and index3 != index2:
           answers.append([index1, index2, index3])
